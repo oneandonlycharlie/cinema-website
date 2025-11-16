f17fb77a5d1d7f34427698d2a5d45b49c4420bfa
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-11-16 23:04:53 +0100

    Add log in page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,68 @@
+import { useState } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
 
-function Login(){
 
-    return (
-        <p>Log in page</p>
-    )
+export default function Login() {
+    const { onLogin } = useOutletContext();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const API_BASE = import.meta.env.VITE_API_BASE_URL;
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    try {
+      const res = await fetch(`${API_BASE}/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+        onLogin(data.user, data.token);  
+        navigate("/");
+      } else {
+        setError(data.message || "Login failed");
+      }
+    } catch (err) {
+      console.error(err);
+      setError("Network error");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email: </label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Password: </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
-
-export default Login;
